e467dbdd0aa6ce4caaae0fd73c288b835387ccd6
Elena <c.elena.palmer@gmail.com>
2023-05-25 07:46:08 -0500

    Add static root to settings

--- ourchive_app/ourchive_app/settings.py
+++ ourchive_app/ourchive_app/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'api.custom_exception_handler.custom_exception_handler',
