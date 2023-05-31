48b085bac210952c2d4f16a615ec1ff6dbb8eb10
Elena <c.elena.palmer@gmail.com>
2023-05-31 16:19:09 -0500

    fix super() reference issue

--- ourchive_app/api/custom_fields.py
+++ ourchive_app/api/custom_fields.py
@@ -9,5 +9,5 @@ class PrivateField(serializers.ReadOnlyField):
 class UserPrivateField(serializers.ReadOnlyField):
     def get_attribute(self, instance):
         if instance == self.context['request'].user:
-            return super(PrivateField, self).get_attribute(instance)
+            return super(UserPrivateField, self).get_attribute(instance)
         return None
