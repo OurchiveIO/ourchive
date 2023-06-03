b41dc159ef8c7ea70a58dea0c362739644ecc7eb
Elena <c.elena.palmer@gmail.com>
2023-06-03 11:44:51 -0500

    add tags to admin console + add searchable columns via ModelAdmin

--- ourchive_app/api/admin.py
+++ ourchive_app/api/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from api.models import TagType, WorkType, NotificationType, OurchiveSetting, ContentPage
+from api.models import TagType, WorkType, NotificationType, OurchiveSetting, ContentPage, Tag
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'display_text', 'tag_type')
+    search_fields = ('text', 'tag_type__label')
 
 admin.site.register(TagType)
 admin.site.register(WorkType)
 admin.site.register(NotificationType)
 admin.site.register(OurchiveSetting)
 admin.site.register(ContentPage)
+admin.site.register(Tag, TagAdmin)
