7e93373cddc15352ee72c29ca05464ad7ff15282
Elena <c.elena.palmer@gmail.com>
2023-06-21 20:00:48 -0500

    rate limiting

--- ourchive_app/ourchive_app/settings.py
+++ ourchive_app/ourchive_app/settings.py
@@ -206,7 +206,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'api.custom_pagination.CustomPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '10000/day'
+    }
 }
 
 LOGGING = {
