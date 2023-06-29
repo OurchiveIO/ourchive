e883c80e498e277923af22b411db92c1182bd5cd
Elena <c.elena.palmer@gmail.com>
2023-06-29 16:14:33 -0500

    Fix css mode logic

--- ourchive_app/frontend/context_processors.py
+++ ourchive_app/frontend/context_processors.py
@@ -6,7 +6,7 @@ from api.utils import convert_boolean
 def set_style(request):
     css_mode = request.session.get('css_mode')
     return {
-        'css_mode': css_mode if css_mode is not None else "light"
+        'css_mode': css_mode if css_mode is not None else None
     }
 
 
