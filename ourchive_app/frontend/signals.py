f57a9db2a26e670ca49227124380420cc55df933
Elena <c.elena.palmer@gmail.com>
2023-08-31 19:32:31 -0500

    work cache expiration on chapter save

--- ourchive_app/frontend/signals.py
+++ ourchive_app/frontend/signals.py
@@ -7,19 +7,24 @@ from django.urls import reverse
 from django.http import HttpRequest
 from django.db import connections
 from django.core.cache.backends.db import DatabaseCache
-
-from api.models import Work
+from api import models as api
 
 def make_key(key, key_prefix, version):
     return f"ourchive:{version}:{key}"
 
-@receiver(post_save, sender=Work)
+@receiver(post_save, sender=api.Work)
 def spoil_cache_create(sender, instance, created, **kwargs):
     if created:
         print('created')
 
-@receiver(post_save, sender=Work)
-def spoil_cache_update(sender, instance, **kwargs):
+@receiver(post_save, sender=api.Work)
+def spoil_work_cache_update(sender, instance, **kwargs):
     connection = connections['default']
     with connection.cursor() as cursor:
         cursor.execute(f"DELETE FROM ourchive_database_cache where cache_key like 'ourchive:%:work_{instance.id}_%'")
+
+@receiver(post_save, sender=api.Chapter)
+def spoil_chapter_cache_update(sender, instance, **kwargs):
+    connection = connections['default']
+    with connection.cursor() as cursor:
+        cursor.execute(f"DELETE FROM ourchive_database_cache where cache_key like 'ourchive:%:work_{instance.work_id}_%'")
