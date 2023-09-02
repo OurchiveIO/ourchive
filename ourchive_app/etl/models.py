07f55313345a77b9df8dbd879c0bf40ee11d2af8
Elena <c.elena.palmer@gmail.com>
2023-09-02 15:36:16 -0500

    Add additional mappings model to etl project

--- ourchive_app/etl/models.py
+++ ourchive_app/etl/models.py
@@ -47,9 +47,31 @@ class ObjectMapping(models.Model):
     object_type = models.CharField(max_length=100, choices=OBJECT_TYPES)
     origin_field = models.CharField(max_length=100)
     destination_field = models.CharField(max_length=100)
+    additional_mappings = models.BooleanField(default=False)
 
     def __repr__(self):
         return '<ObjectMapping: {}>'.format(self.id)
 
     def __str__(self):
         return f"{self.origin_field} to {self.destination_field} for {self.object_type}"
+
+class AdditionalMapping(models.Model):
+    __tablename__ = 'additionalmappings'
+
+    OBJECT_TYPES = [
+        ('tag', 'tag'),
+        ('attribute', 'attribute'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    original_value = models.CharField(max_length=300)
+    destination_object = models.CharField(max_length=100, choices=OBJECT_TYPES)
+    destination_value = models.CharField(max_length=120)
+    destination_type = models.CharField(max_length=200)
+
+    def __repr__(self):
+        return '<AdditionalMapping: {}>'.format(self.id)
+
+    def __str__(self):
+        return f"{self.original_value} to {self.destination_value} for {self.destination_object}"
