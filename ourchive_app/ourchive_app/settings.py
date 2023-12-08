fbdff19f5d07047e8a0fa6477fe973601a6d9328
Elena <c.elena.palmer@gmail.com>
2023-12-07 19:37:07 -0600

    remove unused app

--- ourchive_app/ourchive_app/settings.py
+++ ourchive_app/ourchive_app/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'oauth2_provider',
     'rest_framework',
-    'knox',
     'frontend',
     'django.contrib.postgres',
     'corsheaders',
