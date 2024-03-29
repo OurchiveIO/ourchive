bb32afde56783254c80f2c5efe957aa2e24bec42
c-e-p <c.elena.palmer@outlook.com>
2024-03-28 20:20:35 -0500

    TinyMCE updates + registration age confirmation

--- ourchive_app/frontend/static/frontend/js/ourchive-core.js
+++ ourchive_app/frontend/static/frontend/js/ourchive-core.js
@@ -4,3 +4,19 @@ function removeFile(object) {
 	console.log(document.getElementById(object+'-file-uk-input-original'));
 	document.getElementById(object+'-file-uk-input-original').value = "";
 }
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        cookies.forEach(function (value) { 
+            var cookie = value.trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                return;
+            }
+        });
+    }
+    return cookieValue;
+}
