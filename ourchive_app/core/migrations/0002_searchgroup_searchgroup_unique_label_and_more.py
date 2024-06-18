7e9d867b8d8baad683cb74ad684a005717057c7b
c-e-p <c.elena.palmer@outlook.com>
2024-06-18 14:19:44 -0500

    Add migration from unapplied changes in dev, update core script

--- ourchive_app/core/migrations/0002_searchgroup_searchgroup_unique_label_and_more.py
+++ ourchive_app/core/migrations/0002_searchgroup_searchgroup_unique_label_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
+        ('core', '0002_create_new_anthology_series_and_more'),
     ]
 
     operations = [
