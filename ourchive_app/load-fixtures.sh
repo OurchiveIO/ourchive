cb0879f5faa5c727e32000e774eb2a9267fe9be8
c-e-p <c.elena.palmer@outlook.com>
2024-06-20 18:41:12 -0500

    Update fixtures script, update instructions, update recommended data

--- ourchive_app/load-fixtures.sh
+++ ourchive_app/load-fixtures.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # This loads all the fixtures into the current environment.
-python manage.py loaddata api/fixtures/ourchivesettings.yaml
-python manage.py loaddata api/fixtures/attributetype.yaml
-python manage.py loaddata api/fixtures/attributevalue.yaml
-python manage.py loaddata api/fixtures/notificationtypes.yaml
-python manage.py loaddata api/fixtures/tagtype.yaml
-python manage.py loaddata api/fixtures/worktype.yaml
-python manage.py loaddata etl/fixtures/objectmapping.yaml
+python manage.py loadourchivedata
