8e71636816d147f71b7e338351ab1da76a0213bd
c-e-p <c.elena.palmer@outlook.com>
2025-04-06 21:08:30 -0500

    Docker WIP

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 # Execute pending migrations
 echo Executing pending migrations
-python ourchive_app/manage.py migrate
+python manage.py migrate
 
 # Load default data
 echo Load default data
-python ourchive_app/manage.py loadourchivedata
+python manage.py loadourchivedata
 
 # Give permission to ourchive:ourchive after mounting volumes
 echo Give permission to ourchive:ourchive
@@ -16,7 +16,7 @@ chown -R ourchive:ourchive /ourchive_app
 
 # Start Ourchive processes
 echo Starting Ourchive...
-exec gosu ourchive gunicorn ourchive.wsgi:application \
+exec gosu ourchive gunicorn ourchive_app.wsgi:application \
     --name ourchive \
     --bind 0.0.0.0:8000 \
     --workers 3 \
