8a21ffe829e2e44ab945a845b65588ecfb5885cc
c-e-p <c.elena.palmer@outlook.com>
2025-09-14 20:29:58 -0500

    notes

--- ourchive_app/search/search/search_obj.py
+++ ourchive_app/search/search/search_obj.py
@@ -426,6 +426,7 @@ class SearchOptions(object):
     def __init__(self):
         self.split_include_exclude = False
         self.order_by = '-updated_on'
+        self.subscribed = False
 
     def to_dict(self):
         return self.__dict__
--- ourchive_app/search/search/search_service.py
+++ ourchive_app/search/search/search_service.py
@@ -21,6 +21,7 @@ class OurchiveSearch:
 		if 'work_type_id' in kwargs and kwargs['work_type_id']:
 			return self.filter_by_work_type(**kwargs)
 		if ('work_search') in kwargs:
+			# send user id as param, then pass options to filter builder
 			# TODO: figure out best way to indicate 'we are filtering on user subscriptions' - like tag filter probably?
 			results['work'] = self.searcher.search_works(kwargs['options'], **kwargs['work_search'])
 			tags = tags + results['work'].pop('tags')
